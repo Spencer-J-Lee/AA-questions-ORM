caa46bd49452047b491d36b412efb548475e036f
Spencer <spenjlee@gmail.com>
2019-07-09 19:39:57 -0700

    Add methods to abstract table data as a class instance

--- questions.rb
+++ questions.rb
@@ -14,29 +14,68 @@ end
 class User
 	def self.all
 		users = QuestionsDBConnection.instance.execute("SELECT * FROM users")
+		users.map { |datum| User.new(datum) }
+	end
+
+	def initialize(options)
+		@id = options['id']
+		@fname = options['fname']
+		@lname = options['lname']
 	end
 end
 
 class Question
 	def self.all
 		questions = QuestionsDBConnection.instance.execute("SELECT * FROM questions")
+		questions.map { |datum| Question.new(datum) }
+	end
+	
+	def initialize(options)
+		@id = options['id']
+		@title = options['title']
+		@body = options['body']
+		@author_id = options['author_id']
 	end
 end
 
 class QuestionFollow
 	def self.all
 		question_follows = QuestionsDBConnection.instance.execute("SELECT * FROM question_follows")
+		question_follows.map { |datum| QuestionFollow.new(datum) }
+	end
+	
+	def initialize(options)
+		@id = options['id']
+		@follower_id = options['follower_id']
+		@question_id = options['question_id']
 	end
 end
 
 class QuestionLike
 	def self.all
 		question_likes = QuestionsDBConnection.instance.execute("SELECT * FROM question_likes")
+		question_likes.map { |datum| QuestionLike.new(datum) }
+	end
+	
+	def initialize(options)
+		@id = options['id']
+		@likes = 0
+		@author_id = options['author_id']
+		@question_id = options['question_id']
 	end
 end
 
 class Reply
 	def self.all
 		replies = QuestionsDBConnection.instance.execute("SELECT * FROM replies")
+		replies.map { |datum| Reply.new(datum) }
+	end
+	
+	def initialize(options)
+		@id = options['id']
+		@user_id = options['user_id']
+		@subject_question_id = options['subject_question_id']
+		@parent_reply_id = options['parent_reply_id']
+		@body = options['body']
 	end
 end
