63a45f17c8242d39d2ac214e62c69b047c49a120
Spencer <spenjlee@gmail.com>
2019-07-10 14:52:28 -0700

    Add method to find likers of a specified question

--- question_like.rb
+++ question_like.rb
@@ -1,4 +1,5 @@
 require_relative 'questions_database'
+require_relative 'user'
 
 class QuestionLike
 	def self.all
@@ -19,12 +20,26 @@ class QuestionLike
 		(question_like.empty?) ? nil : QuestionLike.new(question_like.first)
 	end
 
-	attr_accessor :id, :likes, :author_id, :question_id
+	def self.likers_for_question_id(question_id)
+		likers = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+			SELECT
+				users.*
+			FROM
+				users
+			INNER JOIN
+				question_likes ON question_likes.liker_id = users.id
+			WHERE
+				question_id = ?
+		SQL
+
+		(likers.empty?) ? nil : likers.map { |datum| User.new(datum) }
+	end
+
+	attr_accessor :id, :liker_id, :question_id
 
 	def initialize(options)
 		@id = options['id']
-		@likes = 0
-		@author_id = options['author_id']
+		@liker_id = options['liker_id']
 		@question_id = options['question_id']
 	end
 end
