c1745583e3c5c5c78101364623e5765e4304df35
Spencer <spenjlee@gmail.com>
2019-07-11 19:19:22 -0700

    Improve test for ::find_by_name

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -18,14 +18,18 @@ describe User do
 			expect(user.id).to be_nil
 		end
 	end
+	
+end
 
-	describe "::find_by_name" do
-		it "returns user by name" do
-			result = User.find_by_name('First', 'User')
+describe "::find_by_name" do
+	let(:result) { User.find_by_name('First', 'User') }
 
-			expect(result.class).to be(User)
-			expect(result.fname).to eq('First')
-			expect(result.lname).to eq('User')
-		end
+  it "returns an instance of the user class" do
+		expect(result.class).to be(User)
 	end
+
+	it "returns the correct first and last name" do
+    expect(result.fname).to eq('First')
+    expect(result.lname).to eq('User')
+  end
 end
