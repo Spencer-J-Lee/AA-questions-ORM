8c562645d08c25ab1f9618a988f391460ab88550
Spencer <spenjlee@gmail.com>
2019-07-11 17:36:03 -0700

    Create methods for executing and getting last insert row id

--- questions_database.rb
+++ questions_database.rb
@@ -3,10 +3,18 @@ require 'singleton'
 
 class QuestionsDBConnection < SQLite3::Database
 	include Singleton
-	
+
 	def initialize
 		super ('questions.db')
 		self.type_translation = true
 		self.results_as_hash = true
+	end
+
+	def self.execute(*args)
+		instance.execute(*args)
+	end
+
+	def self.last_insert_row_id
+		instance.last_insert_row_id
 	end
 end
